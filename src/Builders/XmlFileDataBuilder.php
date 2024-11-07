0737f06982a0feb12d9847407d644a6720004b35
Marator2K07 <dmitriyshaaa@gmail.com>
2024-11-07 15:24:29 +0300

    Дописываем реализацию для конструктора xml файла.

--- src/Builders/XmlFileDataBuilder.php
+++ src/Builders/XmlFileDataBuilder.php
@@ -25,4 +25,31 @@ class XmlFileDataBuilder implements FileDataBuilder
         return $this;
     }
 
+    public function appendPageDTO(PageDTO $pageDTO): FileDataBuilder
+    {
+        // создаем раздел 
+        $url = $this->xmlData->addChild('url');
+        // и заполняем его
+        foreach ($pageDTO->getData() as $key => $value) {
+            $url->addChild($key, $value);
+        }
+
+        return $this;
+    }
+
+    public function save(string $path = 'file.xml'): bool
+    {
+        // пытаемся получить доступ к папке (создаем, если нужно)
+        $directory = dirname($path);
+        if (!is_dir($directory)) {
+            mkdir($directory, 0755, true);
+        }
+
+        return $this->xmlData->saveXML($path);
+    }
+
+    public function clear(): void
+    {
+        $this->xmlData = new SimpleXMLElement('<urlset/>');
+    }
 }
