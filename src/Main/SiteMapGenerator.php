68fe5ca050098d00584a9d2c4f18e93607a8d02d
Marator2K07 <dmitriyshaaa@gmail.com>
2024-11-07 20:53:31 +0300

    Создан главный класс для генерации карты страниц сайта.
    с основной реализацией.

---
+++ src/Main/SiteMapGenerator.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Src\Main;
+
+use Src\Builders\CsvFileDataBuilder;
+use Src\Builders\JsonFileDataBuilder;
+use Src\Builders\XmlFileDataBuilder;
+use Src\Exceptions\BadPathFileDataBuilderException;
+
+class SiteMapGenerator 
+{
+    private $pagesDTO;
+    private $fileDataBuilder;
+
+    public function __construct(
+        array $pages,
+        string $fileType         
+    ) {
+        // инициализируем конструктор данных файла
+        switch ($fileType) {
+            case 'xml':
+                $this->fileDataBuilder = new XmlFileDataBuilder();
+                break;
+            case 'csv':
+                $this->fileDataBuilder = new CsvFileDataBuilder();
+                break;
+            case 'json':
+                $this->fileDataBuilder = new JsonFileDataBuilder();
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+        // инициализация страниц 
+        foreach ($pages as $page) {
+            $this->pagesDTO[] = new PageDTO($page);
+        }
+    }
+
+    /**
+     * Генерация карты сайта в виде файла заранее указанного формата
+     * @param string $fileName путь для созданного файла карты
+     * @throws BadPathFileDataBuilderException
+     * @return void
+     */
+    public function generate(string $fileName): void
+    {    
+        // "строим" содержимое файла    
+        $this->fileDataBuilder->init([
+            'loc', 'lastmod', 'priority', 'changefreq' 
+        ]);
+        foreach ($this->pagesDTO as $pageDTO) {
+            $this->fileDataBuilder->appendPageDTO($pageDTO);
+        }
+        // и пытаемся сохранить
+        if(!$this->fileDataBuilder->save($fileName)) {
+            throw new BadPathFileDataBuilderException(
+                'Не удалось сохранить карту страниц по указанному пути [' 
+                . $fileName 
+                . ']'
+            );
+        }  
+    }
+}
