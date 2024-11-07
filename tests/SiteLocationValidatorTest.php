fbc8e165926ae5b2f52095f614c4c83e7b08ebf4
Marator2K07 <dmitriyshaaa@gmail.com>
2024-11-07 09:13:01 +0300

    Тесты для валидатора адреса сайта.

---
+++ tests/SiteLocationValidatorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Src\Validators\SiteLocationValidator;
+
+class SiteLocationValidatorTest extends TestCase
+{
+    private $goodUrls;
+    private $badUrls;
+
+    protected function setUp(): void
+    {
+        $this->goodUrls = [
+            "https://site.ru/",
+            "http://site.ru/news",
+            "https://example.com/",
+            "http://my-site.org/about",
+            "https://another-site.net/news/",
+            "http://site-my.en"
+        ];
+        $this->badUrls = [
+            "http:/my-site.org/about",
+            "https:/",
+            "htjp://invalid-site"
+        ];
+    }
+
+    public function testValidate(): void
+    {
+        foreach ($this->goodUrls as $url) {
+            $this->assertTrue(SiteLocationValidator::validate($url));
+        }
+        foreach ($this->badUrls as $url) {
+            $this->assertFalse(SiteLocationValidator::validate($url));
+        }
+    }
+}
