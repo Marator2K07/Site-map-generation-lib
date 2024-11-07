7a6b9ff89526766cac88fe8e2e0ae5bc819829d2
Marator2K07 <dmitriyshaaa@gmail.com>
2024-11-07 13:52:21 +0300

    Создан интерфейс для строителя содержимого файла.

---
+++ src/Interfaces/FileDataBuilder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Src\Interfaces;
+
+use Src\Main\Page\PageDTO;
+
+interface FileDataBuilder 
+{
+    /**
+     * Начальная инициализация данных в зависимости от
+     * конкретного формата наследника
+     * @param array $attributes возможные атрибуты определения данных 
+     * @return FileDataBuilder возврат для поддержания цепочки вызовов
+     */
+    public function init(array $attributes): FileDataBuilder; 
+
+    /**
+     * Добавление отдельного компонента или ребенка
+     * @param string $key ключ для значения
+     * @param string $value значение для добавления
+     * @return FileDataBuilder возврат для поддержания цепочки вызовов
+     */
+    public function append(string $key, string $value): FileDataBuilder;
+
+    /**
+     * Добавление данных из страницы
+     * @param PageDTO $pageDTO данные для добавления
+     * @return FileDataBuilder возврат для поддержания цепочки вызовов
+     */
+    public function appendPageDTO(PageDTO $pageDTO): FileDataBuilder;
+
+    /**
+     * Сохранение построенного содержимого в файл
+     * @return string итоговая строка
+     */
+    public function save(string $path): void;
+
+    /**
+     * Очистка содержимого
+     * @return void
+     */
+    public function clear(): void;
+}
