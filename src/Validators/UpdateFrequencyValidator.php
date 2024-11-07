8838616bda295cef75b286ca32199551de788f60
Marator2K07 <dmitriyshaaa@gmail.com>
2024-11-07 08:00:58 +0300

    Создан новый класс валидации для частоты обновлений.
    с реализацией для основного метода интерфейса.

---
+++ src/Validators/UpdateFrequencyValidator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Src\Validators;
+
+use Src\Interfaces\Validator;
+
+class UpdateFrequencyValidator implements Validator
+{
+    /**
+     * Проверка корректности частоты обновлений
+     * @param mixed $value значение частоты обновлений
+     * @param mixed $pattern массив фильтрации 
+     * @return bool принадлежность массиву фильтрации
+     */
+    public static function validate(        
+        mixed $value,
+        mixed $pattern = [
+            "hourly",
+            "daily",
+            "weekly",
+            "monthly",
+            "annually" // ежегодно
+        ] 
+    ): bool
+    {
+        if (in_array($value, $pattern)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
