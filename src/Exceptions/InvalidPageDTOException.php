2ff6bb1c1406f8bc3f500d1d7fa6a0dbb0628a51
Marator2K07 <dmitriyshaaa@gmail.com>
2024-11-07 09:11:00 +0300

    Простой класс исключений при некорректных значениях данных страницы.

---
+++ src/Exceptions/InvalidPageDTOException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Src\Exceptions;
+
+use Exception;
+use Throwable;
+
+class InvalidPageDTOException extends Exception
+{
+    public function __construct(
+        $message = "",
+        $code = 0,
+        ?Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);        
+    }
+}
